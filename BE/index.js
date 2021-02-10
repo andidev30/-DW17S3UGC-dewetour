9bd1c0bddefc42698cf5245497c956b50fb9f1fe
andidev30 <aandi30082001@gmail.com>
2021-02-10 20:03:37 +0700

    add cors

--- BE/index.js
+++ BE/index.js
@@ -1,21 +1,44 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 const port = 3008;
 
-app.use(cors());
-app.options('*', cors()); 
+var allowedOrigins = [
+  "https://dewetour.andidev.xyz",
+  "https://www.dewetour.andidev.xyz"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+
+    exposedHeaders: ["Content-Length", "X-Foo", "X-Bar"],
+
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.json());
 
 // app.use('/Images', express.static("Images"))
-app.use('/uploads', express.static("uploads"))
+app.use("/uploads", express.static("uploads"));
 
-const routerv1 = require('./routes/routerv1');
-app.use('/api/v1', routerv1)
+const routerv1 = require("./routes/routerv1");
+app.use("/api/v1", routerv1);
 
 app.use("/", (req, res) => {
   res.send("hello-world");
